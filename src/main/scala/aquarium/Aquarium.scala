21472d663aaa743bcd15bc80a5f912dc57d4d065
Elisa Albertini <elisa.albertini3@studio.unibo.it>
2022-08-31 21:40:49 +0200

    Created case class Aquarium. Created tests for Aquarium.

---
+++ src/main/scala/aquarium/Aquarium.scala
@@ -0,0 +1,38 @@
+package aquarium
+
+/** This class represent the current state of the simulation
+  *
+  * @param aquariumState
+  *   represent the current state of the aquarium
+  * @param population
+  *   represent the current population of the aquarium
+  */
+case class Aquarium(aquariumState: AquariumState, population: Population)
+
+/** Companion object of the case class */
+object Aquarium:
+
+  /** Create a new [[Aquarium]]
+    *
+    * @param herbivorousFishesNumber
+    *   number of herbivorous fishes
+    * @param carnivorousFishesNumber
+    *   number of carnivorous fishes
+    * @param algaeNumber
+    *   number of algae
+    * @return
+    *   an instance of the simulation (the initial one)
+    */
+  def apply(herbivorousFishesNumber: Int, carnivorousFishesNumber: Int, algaeNumber: Int): Aquarium =
+    val aquariumState = AquariumState(
+      InitializeAquarium.TEMPERATURE,
+      InitializeAquarium.BRIGHTNESS,
+      InitializeAquarium.PH,
+      InitializeAquarium.IMPURITY,
+      InitializeAquarium.OXYGENATION
+    )
+
+    Aquarium(
+      aquariumState = aquariumState,
+      population = Population(herbivorousFishesNumber, carnivorousFishesNumber, algaeNumber)
+    )
---
+++ src/test/scala/TestAquarium/TestAquarium.scala
@@ -0,0 +1,46 @@
+package TestAquarium
+
+import aquarium.*
+import org.scalatest.funspec.AnyFunSpec
+
+import scala.runtime.stdLibPatches.Predef.assert
+
+/** Test for [[Aquarium]] */
+class TestAquarium extends AnyFunSpec:
+
+  val herbivorousFishesNumber = 3
+  val carnivorousFishesNumber = 4
+  val algaeNumber = 5
+
+  val aquarium: Aquarium = Aquarium(
+    herbivorousFishesNumber = herbivorousFishesNumber,
+    carnivorousFishesNumber = carnivorousFishesNumber,
+    algaeNumber = algaeNumber
+  )
+
+  describe("A new Aquarium") {
+    describe("when initialized") {
+
+      describe("with a given aquariumState") {
+        it(s"should have temperature equals to INITIAL_TEMPERATURE") {
+          assert(aquarium.aquariumState.temperature == InitializeAquarium.TEMPERATURE)
+        }
+
+        it(s"should have brightness equals to INITIAL_BRIGHTNESS") {
+          assert(aquarium.aquariumState.brightness == InitializeAquarium.BRIGHTNESS)
+        }
+
+        it(s"should have ph equals to INITIAL_PH") {
+          assert(aquarium.aquariumState.ph == InitializeAquarium.PH)
+        }
+
+        it(s"should have impurity equals to INITIAL_IMPURITY") {
+          assert(aquarium.aquariumState.impurity == InitializeAquarium.IMPURITY)
+        }
+
+        it(s"should have oxygenation equals to INITIAL_OXYGENATION") {
+          assert(aquarium.aquariumState.oxygenation == InitializeAquarium.OXYGENATION)
+        }
+      }
+    }
+  }

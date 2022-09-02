cc1ff4d97e7896fa389ed68d7c1856c0b56774e2
Ledmington <ledmington.dev@gmail.com>
2022-09-02 10:38:50 +0200

    Defined Fish requirements

---
+++ src/test/scala/TestFish.scala
@@ -0,0 +1,33 @@
+import org.scalatest.funspec.AnyFunSpec
+import org.scalatest.BeforeAndAfterEach
+
+import model.Fish
+
+class TestFish extends AnyFunSpec with BeforeAndAfterEach:
+
+  var f: Fish = Fish()
+
+  override def beforeEach(): Unit =
+    f = Fish()
+
+  describe("A new Fish") {
+    it("should have age 0") {
+      assert(f.age == 0)
+    }
+
+    it("should have max hunger") {
+      assert(f.hunger == Fish.MAX_HUNGER)
+    }
+
+    it("should not be dead") {
+      assert(!f.isDead)
+    }
+
+    it("should have speed 0") {
+      assert(f.speed == (0.0, 0.0))
+    }
+
+    it("should not have an empty name") {
+      assert(f.name != "")
+    }
+  }

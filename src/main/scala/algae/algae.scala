9b49d5b0960b5a756c3ee2af31493f9adaae7635
Filippo Benvenuti <filippo.benvenuti3@studio.unibo.it>
2022-09-02 12:13:24 +0200

    Algae extends Entity + scaladoc

--- src/main/scala/algae/algae.scala
+++ src/main/scala/algae/algae.scala
@@ -1,6 +1,21 @@
-case class Algae(height: Int = 1):
-    val oxygenProduction: Int = Algae.OXYGEN_MULTIPLIER * height
+package Algae
+
+import Model.Entity
+
+/**
+  * Algae model definition.
+  *
+  * @param base Starting and definitive base position (Y will always be 0).
+  * @param height Starting height (1 by default).
+  */
+class Algae(base: Double, val height: Int = Algae.DEFAULT_HEIGHT) extends Entity:
+    val position: (Double, Double) = (base, 0)
+    val oxygenShift: Double = Algae.OXYGEN_MULTIPLIER * height
+    val impurityShift: Double  = 0.0
+    val phShift: Double = height / Algae.PH_DIVISOR
 
 object Algae:
+    val DEFAULT_HEIGHT: Int = 1
     val MAX_HEIGHT: Int = 100
-    val OXYGEN_MULTIPLIER: Int = 2
+    val OXYGEN_MULTIPLIER: Double = 2
+    val PH_DIVISOR: Double = 10

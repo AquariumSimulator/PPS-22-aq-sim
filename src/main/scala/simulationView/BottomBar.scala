91375bdacf29f8a5905bd951f0aa9f6366ad14e8
Pedoni <emanuele.lamagna@studio.unibo.it>
2022-09-02 11:37:15 +0200

    Modified GUI ControlBar

--- src/main/scala/simulationView/BottomBar.scala
+++ src/main/scala/simulationView/BottomBar.scala
@@ -2,13 +2,23 @@ package simulationView
 
 import scalafx.scene.layout.*
 import scalafx.scene.control.Button
+import scalafx.geometry.Pos
+import scalafx.geometry.Insets
 
 import scala.language.postfixOps
 
 object BottomBar:
+    val buttonsNumber = 5
+
     val bottomBar = new TilePane:
-        children += new Button("1")
-        children += new Button("2")
-        children += new Button("3")
-        children += new Button("4")
-        children += new Button("5")
+        for (a <- 1 to buttonsNumber)
+            children += new Button:
+                text = a.toString
+                margin = Insets.apply(
+                    top = 10,
+                    right = 10,
+                    bottom = 10,
+                    left = 10
+                )
+
+    bottomBar.alignment = Pos.Center

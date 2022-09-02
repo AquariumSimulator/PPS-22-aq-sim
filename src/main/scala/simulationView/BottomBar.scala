edfd499619c9e3db0bff40c4e65e4a6fac6d8854
Pedoni <emanuele.lamagna@studio.unibo.it>
2022-09-02 15:01:42 +0200

    BottomBar style changed

--- src/main/scala/simulationView/BottomBar.scala
+++ src/main/scala/simulationView/BottomBar.scala
@@ -4,16 +4,20 @@ import scalafx.scene.layout.*
 import scalafx.scene.control.Button
 import scalafx.geometry.Pos
 import scalafx.geometry.Insets
+import scalafx.scene.paint.Color
+import scalafx.scene.shape.Circle
 
 import scala.language.postfixOps
 
 object BottomBar:
     val buttonsNumber = 5
-
     val bottomBar = new TilePane:
+        background = new Background(Array(new BackgroundFill(Color.Grey, null, null)))
         for (a <- 1 to buttonsNumber)
             children += new Button:
+                //shape = new Circle
                 text = a.toString
+                background = new Background(Array(new BackgroundFill(Color.Yellow, null, null)))
                 margin = Insets.apply(
                     top = 10,
                     right = 10,

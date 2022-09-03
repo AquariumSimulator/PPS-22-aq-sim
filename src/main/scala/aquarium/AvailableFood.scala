538e432181ef4ba25fdc3419a8bee660dd4e67ec
Elisa Albertini <elisa.albertini3@studio.unibo.it>
2022-09-04 00:02:35 +0200

    Created trait UpdateAvailableFood (with its companion object). UpdateAvailableFood is meant to update a set of Food adding or removing elements.

--- src/main/scala/aquarium/AvailableFood.scala
+++ src/main/scala/aquarium/AvailableFood.scala
@@ -1,6 +1,6 @@
 package aquarium
 
-import model.{CarnivorousFood, HerbivorousFood}
+import model.{CarnivorousFood, Food, HerbivorousFood}
 
 /** This case class represent the food available inside the aquarium
   *
@@ -10,3 +10,43 @@ import model.{CarnivorousFood, HerbivorousFood}
   *   carnivorous food available
   */
 case class AvailableFood(herbivorousFood: Seq[HerbivorousFood], carnivorousFood: Seq[CarnivorousFood])
+
+/** Trait that models methods for modifying he food available inside the aquarium */
+trait UpdateAvailableFood:
+
+  /** Add a new food
+    * @param elem
+    * @return
+    *   a new seq of [[Food]]
+    */
+  def addFood(elem: Food): Seq[Food]
+
+  /** Remove a particular instance of food
+    * @param elem
+    *   that have to be removed
+    * @return
+    *   a new seq of [[Food]]
+    */
+  def deleteFood(elem: Food): Seq[Food]
+
+/** Companion object of [[UpdateAvailableFood]] */
+object UpdateAvailableFood:
+
+  /** Create a new [[UpdateAvailableFood]] given a seq of [[Food]]
+    * @param seq
+    *   seq of [[Food]]
+    * @return
+    *   a new [[UpdateAvailableFood]]
+    */
+  def apply(seq: Seq[Food]): UpdateAvailableFood = UpdateAvailableFoodImpl(seq)
+
+  /** Hidden implementation of [[UpdateAvailableFood]]
+    * @param seq
+    *   seq of [[Food]]
+    */
+  private class UpdateAvailableFoodImpl(seq: Seq[Food]) extends UpdateAvailableFood:
+
+    override def addFood(elem: Food): Seq[Food] =
+      seq :+ elem
+
+    override def deleteFood(elem: Food): Seq[Food] = ???

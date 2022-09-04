082a98e5e661eafca84980b4b52e97a0d72e9187
Ledmington <ledmington.dev@gmail.com>
2022-09-04 16:40:01 +0200

    Formatting and fixed indentation in GUI

--- src/main/scala/simulationView/widgets/SimulationViewer.scala
+++ src/main/scala/simulationView/widgets/SimulationViewer.scala
@@ -9,39 +9,39 @@ import scalafx.scene.image.Image
 import scala.util.Random
 
 object SimulationViewer:
-    val canvas = new BorderPane:
-        val canv = new Canvas:
-            width = 600
-            height = 400
-            background = new Background(
-                Array(
-                    new BackgroundFill(
-                        new LinearGradient(
-                            endX = 0,
-                            stops = Stops(
-                                Color.rgb(0,191,255),
-                                Color.rgb(25,25,112),
-                            )
-                        ),
-                        //Color.rgb(171, 205, 239),
-                        null,
-                        null,
-                    )
-                )
-            )
-            margin = Insets.apply(
-                top = 30,
-                right = 30,
-                bottom = 30,
-                left = 30
-            )
-        center = canv
-        
-        val gc = canv.graphicsContext2D
+  val canvas = new BorderPane:
+    val canv = new Canvas:
+      width = 600
+      height = 400
+      background = new Background(
+        Array(
+          new BackgroundFill(
+            new LinearGradient(
+              endX = 0,
+              stops = Stops(
+                Color.rgb(0, 191, 255),
+                Color.rgb(25, 25, 112)
+              )
+            ),
+            //Color.rgb(171, 205, 239),
+            null,
+            null
+          )
+        )
+      )
+      margin = Insets.apply(
+        top = 30,
+        right = 30,
+        bottom = 30,
+        left = 30
+      )
+    center = canv
 
-        val greenFish = new Image("/green-fish.png")
-        val redFish = new Image("/red-fish.png")
+    val gc = canv.graphicsContext2D
 
-        val rnd = new Random()
-        for (n <- 1 to 10) gc.drawImage(greenFish, rnd.nextDouble() * 500, rnd.nextDouble() * 400, 60, 50)
-        for (n <- 1 to 10) gc.drawImage(redFish, rnd.nextDouble() * 500, rnd.nextDouble() * 400, 60, 50)
+    val greenFish = new Image("/green-fish.png")
+    val redFish = new Image("/red-fish.png")
+
+    val rnd = new Random()
+    for (n <- 1 to 10) gc.drawImage(greenFish, rnd.nextDouble() * 500, rnd.nextDouble() * 400, 60, 50)
+    for (n <- 1 to 10) gc.drawImage(redFish, rnd.nextDouble() * 500, rnd.nextDouble() * 400, 60, 50)

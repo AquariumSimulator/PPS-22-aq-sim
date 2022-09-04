6919ff0334b3c335bdce6cc5a6d3f9cb36f1a796
Elisa Albertini <elisa.albertini3@studio.unibo.it>
2022-09-05 00:39:03 +0200

    Added tests In TestUpdatePopulation due to the new implementation of the function that remove elements from the Population seq.

--- src/test/scala/aquarium/TestUpdatePopulation.scala
+++ src/test/scala/aquarium/TestUpdatePopulation.scala
@@ -1,8 +1,8 @@
 package aquarium
 
-import model.CarnivorousFood
 import model.aquarium.{Population, UpdateSpecificPopulation}
 import model.fish.{CarnivorousFish, HerbivorousFish}
+import model.{Algae, CarnivorousFood}
 import org.scalatest.funspec.AnyFunSpec
 
 import scala.runtime.stdLibPatches.Predef.assert
@@ -14,17 +14,28 @@ class TestUpdatePopulation extends AnyFunSpec:
   val herbivorousFishesNumber = 3
   val carnivorousFishesNumber = 4
   val algaeNumber = 5
+  val algaeBase = 1.0
 
   val population = Population(herbivorousFishesNumber, carnivorousFishesNumber, algaeNumber)
 
   val addElementHerbivorous = HerbivorousFish()
   val addElementCarnivorous = CarnivorousFish()
+  val addElementAlgae = Algae(algaeBase)
 
   val updateHerbivorousAdd = UpdateSpecificPopulation(population.herbivorous)
-  val updateCarnivorousAdd = UpdateSpecificPopulation(population.carnivorous)
-
   val newSeqHerbivorousAdd = updateHerbivorousAdd + addElementHerbivorous
+  val updateHerbivorousRemove = UpdateSpecificPopulation(newSeqHerbivorousAdd)
+  val newSetHerbivorousRemove = updateHerbivorousRemove - addElementHerbivorous
+
+  val updateCarnivorousAdd = UpdateSpecificPopulation(population.carnivorous)
   val newSeqCarnivorousAdd = updateCarnivorousAdd + addElementCarnivorous
+  val updateCarnivorousRemove = UpdateSpecificPopulation(newSeqCarnivorousAdd)
+  val newSetCarnivorousRemove = updateCarnivorousRemove - addElementCarnivorous
+
+  val updateAlgaeAdd = UpdateSpecificPopulation(population.algae)
+  val newSeqAlgaeAdd = updateAlgaeAdd + addElementAlgae
+  val updateAlgaeRemove = UpdateSpecificPopulation(newSeqAlgaeAdd)
+  val newSetAlgaeRemove = updateAlgaeRemove - addElementAlgae
 
   describe("A new UpdatePopulation object") {
     describe("with a given seq of elements (seq of herbivorous fish)") {
@@ -38,6 +49,16 @@ class TestUpdatePopulation extends AnyFunSpec:
           }
         }
       }
+      describe("once remove method is called") {
+        describe("return a new set that") {
+          it("should have size equal to the old size") {
+            assert(newSetHerbivorousRemove.size == population.herbivorous.size)
+          }
+          it(s"should not contain $addElementHerbivorous") {
+            assert(!newSetHerbivorousRemove.contains(addElementHerbivorous))
+          }
+        }
+      }
     }
     describe("with a given seq of elements (seq of carnivorous fish)") {
       describe("once add method is called") {
@@ -47,6 +68,38 @@ class TestUpdatePopulation extends AnyFunSpec:
           }
           it(s"should contain $addElementCarnivorous") {
             assert(newSeqCarnivorousAdd.contains(addElementCarnivorous))
+          }
+        }
+      }
+      describe("once remove method is called") {
+        describe("return a new set that") {
+          it("should have size equal to the old size") {
+            assert(newSetCarnivorousRemove.size == population.carnivorous.size)
+          }
+          it(s"should not contain $addElementCarnivorous") {
+            assert(!newSetCarnivorousRemove.contains(addElementCarnivorous))
+          }
+        }
+      }
+    }
+    describe("with a given seq of elements (seq of algae)") {
+      describe("once add method is called") {
+        describe("return a new Seq that") {
+          it("should have size equal to the old size + 1") {
+            assert(newSeqAlgaeAdd.size == population.algae.size + 1)
+          }
+          it(s"should contain $addElementAlgae") {
+            assert(newSeqAlgaeAdd.contains(addElementAlgae))
+          }
+        }
+      }
+      describe("once remove method is called") {
+        describe("return a new set that") {
+          it("should have size equal to the old size") {
+            assert(newSetAlgaeRemove.size == population.algae.size)
+          }
+          it(s"should not contain $addElementAlgae") {
+            assert(!newSetAlgaeRemove.contains(addElementAlgae))
           }
         }
       }

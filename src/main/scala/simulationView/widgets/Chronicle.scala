66f71ed533ddb0bc31c16cdee54b197ca514f827
Pedoni <emanuele.lamagna@studio.unibo.it>
2022-09-04 18:28:35 +0200

    Added info section

--- src/main/scala/simulationView/widgets/Chronicle.scala
+++ src/main/scala/simulationView/widgets/Chronicle.scala
@@ -8,37 +8,22 @@ import scalafx.geometry.Pos
 import scalafx.geometry.Insets
 
 object Chronicle:
-	val chronicle = new BorderPane:
-		margin = Insets.apply(
-			top = 5,
-			right = 0,
-			bottom = 0,
-			left = 0
-		)
-		background = new Background(Array(new BackgroundFill(Color.White, null, null)))
-		top = new Label:
-			alignmentInParent = Pos.Center
-			text = "Chronicle"
-		center = new ListView(
-			items = Seq(
-				"Fish1 is dead",
-				"Fish2 ate Fish3",
-				"Fish4 was born",
-				"Fish2 ate Fish3",
-				"Fish4 was born",
-				"Fish2 ate Fish3",
-				"Fish4 was born",
-				"Fish2 ate Fish3",
-				"Fish4 was born",
-				"Fish2 ate Fish3",
-				"Fish4 was born",
-				"Fish2 ate Fish3",
-				"Fish4 was born",
-				"Fish2 ate Fish3",
-				"Fish4 was born",
-				"Fish2 ate Fish3",
-				"Fish4 was born",
-				"Fish2 ate Fish3",
-				"Fish4 was born"
-			)
-		)
+  val chronicle = new BorderPane:
+    margin = Insets.apply(
+      top = 5,
+      right = 0,
+      bottom = 0,
+      left = 0
+    )
+    background = new Background(Array(new BackgroundFill(Color.White, null, null)))
+    top = new Label:
+      alignmentInParent = Pos.Center
+      text = "Chronicle"
+    center = new ListView(
+      items = Seq(
+        "Fish1 is dead", "Fish2 ate Fish3", "Fish4 was born", "Fish2 ate Fish3", "Fish4 was born", "Fish2 ate Fish3",
+        "Fish4 was born", "Fish2 ate Fish3", "Fish4 was born", "Fish2 ate Fish3", "Fish4 was born", "Fish2 ate Fish3",
+        "Fish4 was born", "Fish2 ate Fish3", "Fish4 was born", "Fish2 ate Fish3", "Fish4 was born", "Fish2 ate Fish3",
+        "Fish4 was born"
+      )
+    )

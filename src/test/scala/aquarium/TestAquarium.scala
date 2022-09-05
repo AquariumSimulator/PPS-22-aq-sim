546c5de9807a394303fbd04d644d19ccca273a97
Elisa Albertini <elisa.albertini3@studio.unibo.it>
2022-09-05 19:21:37 +0200

    Refactoring TestAquarium: renamed variables.

--- src/test/scala/aquarium/TestAquarium.scala
+++ src/test/scala/aquarium/TestAquarium.scala
@@ -22,33 +22,33 @@ class TestAquarium extends AnyFunSpec:
     describe("when initialized") {
 
       describe("with a given aquariumState") {
-        it(s"should have temperature equals to INITIAL_TEMPERATURE") {
+        it(s"should have temperature equals to ${InitializeAquarium.TEMPERATURE}") {
           assert(aquarium.aquariumState.temperature == InitializeAquarium.TEMPERATURE)
         }
 
-        it(s"should have brightness equals to INITIAL_BRIGHTNESS") {
+        it(s"should have brightness equals to ${InitializeAquarium.BRIGHTNESS}") {
           assert(aquarium.aquariumState.brightness == InitializeAquarium.BRIGHTNESS)
         }
 
-        it(s"should have ph equals to INITIAL_PH") {
+        it(s"should have ph equals to ${InitializeAquarium.PH}") {
           assert(aquarium.aquariumState.ph == InitializeAquarium.PH)
         }
 
-        it(s"should have impurity equals to INITIAL_IMPURITY") {
+        it(s"should have impurity equals to ${InitializeAquarium.IMPURITY}") {
           assert(aquarium.aquariumState.impurity == InitializeAquarium.IMPURITY)
         }
 
-        it(s"should have oxygenation equals to INITIAL_OXYGENATION") {
+        it(s"should have oxygenation equals to ${InitializeAquarium.OXYGENATION}") {
           assert(aquarium.aquariumState.oxygenation == InitializeAquarium.OXYGENATION)
         }
       }
 
-      describe("with a given availableFood") {
-        it("should have herbivorousFood set empty") {
+      describe("with a given availableFood that") {
+        it("should have an empty herbivorous food set") {
           assert(aquarium.availableFood.herbivorousFood.isEmpty)
         }
 
-        it("should have carnivorousFood set empty") {
+        it("should have an empty carnivorous food set") {
           assert(aquarium.availableFood.carnivorousFood.isEmpty)
         }
       }

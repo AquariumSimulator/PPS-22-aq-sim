7ef09b134a699ea90442bd265500793db87dd932
Filippo Barbari <filippo.barbari@studio.unibo.it>
2022-09-07 16:08:55 +0200

    Adjusted fonts in InfoPane

--- src/main/scala/simulationView/widgets/InfoCell.scala
+++ src/main/scala/simulationView/widgets/InfoCell.scala
@@ -1,16 +1,18 @@
 package simulationView.widgets
 
-import scalafx.scene.layout.TilePane
+import scalafx.scene.layout.BorderPane
 import scalafx.scene.control.Label
 import scalafx.geometry.Pos
+import scalafx.scene.text.FontWeight
+import scalafx.scene.text.Font
+import scalafx.scene.text.FontPosture
 
-class InfoCell(val title: String, var content: String) extends TilePane:
-  children ++= Seq(
-    new Label:
-      alignmentInParent = Pos.TopCenter
-      text = title
-    ,
-    new Label:
-      alignmentInParent = Pos.BottomCenter
-      text = content
-  )
+class InfoCell(val title: String, val content: String) extends BorderPane:
+  top = new Label:
+    alignmentInParent = Pos.Center
+    text = title
+    font = Font.font("Helvetica", FontWeight.Bold, FontPosture.Regular, 10.0)
+  bottom = new Label:
+    alignmentInParent = Pos.Center
+    text = content
+    font = Font.font("Helvetica", FontWeight.Normal, FontPosture.Regular, 15.0)

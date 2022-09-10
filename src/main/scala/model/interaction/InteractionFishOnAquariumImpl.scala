d89d719793dbe1afaa952aac1fc30c02e5155bee
Elisa Albertini <elisa.albertini3@studio.unibo.it>
2022-09-10 23:41:06 +0200

    Implemented InteractionFishOnAquariumImpl: calculate the new oxygenation, ph and impurity level of the aquarium.

---
+++ src/main/scala/model/interaction/InteractionFishOnAquariumImpl.scala
@@ -0,0 +1,30 @@
+package model.interaction
+
+import model.aquarium.{AquariumParametersLimits, AquariumState}
+import model.fish.{Fish, HerbivorousFish}
+import model.interaction.Interaction
+
+/** Hidden implementation of [[Interaction]]
+  * @param aquariumState
+  *   the current state of the aquarium that have to be updated
+  * @param fish
+  *   a [[Fish]] of the aquarium that influences the aquarium state
+  */
+private class InteractionFishOnAquariumImpl(aquariumState: AquariumState, fish: Fish)
+    extends Interaction[AquariumState]:
+
+  override def update(): AquariumState =
+
+    val newImpurity = aquariumState.impurity + fish.impurityShift match
+      case impurity if impurity <= AquariumParametersLimits.IMPURITY_MAX => impurity
+      case _ => aquariumState.impurity
+
+    val newOxygenation = aquariumState.oxygenation + fish.oxygenShift match
+      case oxygen if oxygen >= AquariumParametersLimits.OXYGENATION_MIN => oxygen
+      case _ => aquariumState.oxygenation
+
+    val newPh = aquariumState.ph + fish.phShift match
+      case ph if ph <= AquariumParametersLimits.PH_MAX => ph
+      case _ => aquariumState.ph
+
+    aquariumState.copy(impurity = newImpurity, oxygenation = newOxygenation, ph = newPh)

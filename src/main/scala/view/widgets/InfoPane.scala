0750ef4ea02226f329b1fa23d1b2993f11f599a5
Ledmington <ledmington.dev@gmail.com>
2022-09-11 16:07:46 +0200

    Added tooltips to InfoPane buttons

--- src/main/scala/view/widgets/InfoPane.scala
+++ src/main/scala/view/widgets/InfoPane.scala
@@ -1,21 +1,32 @@
 package view.widgets
 
 import scalafx.geometry.{Insets, Pos}
-import scalafx.scene.control.{Button, Label}
+import scalafx.scene.control.{Button, Label, Tooltip}
 import scalafx.scene.layout.*
 import scalafx.scene.paint.Color
 import view.utils.{AquariumFonts, IconButton}
+import scalafx.event.ActionEvent
+import scalafx.Includes._
 
 object InfoPane:
+
+  val statisticsButton: IconButton = IconButton("chart.png")
+  statisticsButton.tooltip = new Tooltip("View statistics")
+  statisticsButton.onAction = (event: ActionEvent) => println("Clicked view statistics")
+
+  val downloadButton: IconButton = IconButton("download.png")
+  downloadButton.tooltip = new Tooltip("Download simulation data")
+  downloadButton.onAction = (event: ActionEvent) => println("Clicked download data")
+
   val pane = new BorderPane:
     padding = Insets(10, 10, 10, 10)
     background = new Background(Array(new BackgroundFill(Color.White, null, null)))
     top = new BorderPane:
-      left = IconButton("/chart.png")
+      left = statisticsButton
       center = new Label:
         text = "Info"
         font = AquariumFonts.bold(15.0)
-      right = IconButton("/download.png")
+      right = downloadButton
     bottom = new GridPane:
       alignment = Pos.Center
       hgap = 40

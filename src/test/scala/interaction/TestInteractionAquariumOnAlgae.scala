e9575fe9152f38083dfb78b413a20f7e021f0c4c
Elisa Albertini <elisa.albertini3@studio.unibo.it>
2022-09-12 19:08:39 +0200

    Refactoring.

--- src/test/scala/interaction/TestInteractionAquariumOnAlgae.scala
+++ src/test/scala/interaction/TestInteractionAquariumOnAlgae.scala
@@ -7,20 +7,18 @@ import org.scalatest.funspec.AnyFunSpec
 
 import scala.runtime.stdLibPatches.Predef.assert
 
+/** Test for the interaction of the aquarium state on an algae */
 class TestInteractionAquariumOnAlgae extends AnyFunSpec:
 
   private val aquariumStateAlgaeAlive = AquariumState(5, 50, 7, 10, 10)
-  private val aquariumStateAlgaeDead = AquariumState(5, 3, 7, 10, 10)
 
   private val algaeAliveThatCanGrow = Algae(0, Algae.MAX_HEIGHT / 2)
   private val algaeAliveThatCantGrow = Algae(1, Algae.MAX_HEIGHT)
-  private val algaeDead = Algae(1)
 
   private val interactionAquariumAlgaeForAliveAndGrowTest =
     Interaction(algaeAliveThatCanGrow, aquariumStateAlgaeAlive)
   private val interactionAquariumAlgaeForAliveAndNotGrowTest =
     Interaction(algaeAliveThatCantGrow, aquariumStateAlgaeAlive)
-  private val interactionAquariumAlgaeForDeadTest = Interaction(algaeDead, aquariumStateAlgaeDead)
 
   private val expectedGrowth = 5
 
@@ -28,7 +26,6 @@ class TestInteractionAquariumOnAlgae extends AnyFunSpec:
     describe("with a given Algae") {
       describe("and a given AquariumState") {
         describe("when update() is called") {
-          //TODO test death (?)
           describe(s"if the brightness level of the AquariumState is greater than ${Algae.LOWER_BRIGHTNESS_LEVEL}") {
             it("it should return a new Algae updated") {
               assert(interactionAquariumAlgaeForAliveAndGrowTest.update().isDefined)

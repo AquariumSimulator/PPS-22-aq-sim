ac274002c340f6e8e2475bb7c3afec8c1379d274
Ledmington <ledmington.dev@gmail.com>
2022-09-14 22:36:28 +0200

    Added sample prolog theory and query

--- src/main/scala/model/db/database.scala
+++ src/main/scala/model/db/database.scala
@@ -1,8 +1,25 @@
 package model.db
 
 import alice.tuprolog.{Prolog, Theory}
-import java.io.InputStream
+import alice.tuprolog.SolveInfo
+import model.fish.Fish
 
 object PrologEngine:
   val engine = new Prolog()
-  engine.setTheory(new Theory(getClass.getResource("/mainTheory.pl").openStream()))
+  engine.setTheory(new Theory(getClass.getResource("/prolog/mainTheory.pl").openStream()))
+
+  engine.addTheory(new Theory("parent(banana, pippo)."))
+  engine.addTheory(new Theory("parent(pippo, pera)."))
+  engine.addTheory(new Theory("parent(bananino, pera)."))
+  engine.addTheory(new Theory("parent(cipolla, bananino)."))
+
+  println(engine.getTheory())
+
+  println(engine.solve("parent(X, Y)."))
+  while (engine.hasOpenAlternatives())
+    println(engine.solveNext())
+
+trait PrologEngine:
+  def saveFish(f: Fish): Unit
+  def saveSonOf(parent: Fish, son: Fish): Unit
+//def getGenealogicalTreeOf(f: Fish): Unit

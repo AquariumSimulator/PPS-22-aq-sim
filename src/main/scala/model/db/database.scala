1de05f28934f7464494c6b080b9efc7e76ba9c90
Ledmington <ledmington.dev@gmail.com>
2022-09-16 15:16:59 +0200

    Minor formatting

--- src/main/scala/model/db/database.scala
+++ src/main/scala/model/db/database.scala
@@ -10,12 +10,14 @@ import model.CarnivorousFood
 
 /** Database methods to store and retrieve information. */
 trait PrologEngine:
+
   /** Save all the information of passed fish. If the fish already exists informations are updated.
     *
     * @param fish
     *   The fish to be saved.
     */
   def saveFish(fish: Fish): Unit
+
   /** Save a new relationship of parenting between two fishes.
     *
     * @param parent
@@ -24,12 +26,14 @@ trait PrologEngine:
     *   The son fish.
     */
   def saveSonOf(parent: Fish, son: Fish): Unit
+
   /** Save all the information of passed algae. If the algae already exists informations are updated.
     *
     * @param algae
     *   The algae to be saved.
     */
   def saveAlgae(algae: Algae): Unit
+
   /** Save all the information of passed herbivorous food. If the herbivorous food already exists informations are
     * updated.
     *
@@ -37,6 +41,7 @@ trait PrologEngine:
     *   The herbivorous food to be saved.
     */
   def saveHerbFood(herbFood: HerbivorousFood): Unit
+
   /** Save all the information of passed carnivorous food. If the carnivorous food already exists informations are
     * updated.
     *
@@ -44,24 +49,28 @@ trait PrologEngine:
     *   The carnivorous food to be saved.
     */
   def saveCarnFood(carnFood: CarnivorousFood): Unit
+
   /** Retrieve all saved fish in the simulation.
     *
     * @return
     *   An immutable list of fish.
     */
   def getAllFish: List[Fish]
+
   /** Retrieve all saved algae in the simulation.
     *
     * @return
     *   An immutable list of algae.
     */
   def getAllAlgae: List[Algae]
+
   /** Retrieve all saved herbivorous food in the simulation.
     *
     * @return
     *   An immutable list of herbivorous food.
     */
   def getAllHerbFood: List[HerbivorousFood]
+
   /** Retrieve all saved carnivorous food in the simulation.
     *
     * @return

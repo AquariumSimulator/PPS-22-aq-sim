1c06d6410befcd3ee005982dee55c90fc0c01601
Elisa Albertini <elisa.albertini3@studio.unibo.it>
2022-09-17 15:22:53 +0200

    Fix Alga.

--- src/main/scala/model/Algae.scala
+++ src/main/scala/model/Algae.scala
@@ -7,7 +7,7 @@ package model
   * @param height
   *   Starting height (1 by default).
   */
-case class Algae(base: Double, val height: Int = Algae.DEFAULT_HEIGHT) extends Entity:
+case class Algae(base: Double, height: Int = Algae.DEFAULT_HEIGHT) extends Entity:
   val position: (Double, Double) = (base, 0)
   val oxygenShift: Double = Algae.OXYGEN_MULTIPLIER * height
   val impurityShift: Double = 0.0
@@ -15,7 +15,7 @@ case class Algae(base: Double, val height: Int = Algae.DEFAULT_HEIGHT) extends E
 
 object Algae:
   val DEFAULT_HEIGHT: Int = 1
-  val MAX_HEIGHT: Int = 100
+  val MAX_HEIGHT: Int = 20
   val OXYGEN_MULTIPLIER: Double = 0.2
   val PH_DIVISOR: Double = 10
   val LOWER_BRIGHTNESS_LEVEL: Int = 5

47fc7bcd177da144aabe0b167bd27247fc04f8fb
Elisa Albertini <elisa.albertini3@studio.unibo.it>
2022-09-17 10:05:56 +0200

    Added methods in Model.

--- src/main/scala/model/Model.scala
+++ src/main/scala/model/Model.scala
@@ -1,6 +1,29 @@
 package model
 
+import model.aquarium.{Aquarium, AquariumState, AvailableFood, Population}
+import model.fish.Fish
+
 /** Model trait implemented in [[ModelImpl]]. */
 trait Model:
 
   def m(): Int
+
+  def initializeAquarium(herbivorousFishNumber: Int, carnivorousFishNumber: Int, algaeNumber: Int): Aquarium
+  
+  def updateTemperatureByUser(temperature: Double, aquarium: Aquarium): Aquarium
+
+  def updateBrightnessByUser(brightness: Double, aquarium: Aquarium): Aquarium
+
+  def cleanByUser(aquarium: Aquarium): Aquarium
+
+  def updateOxygenationByUser(oxygenation: Double, aquarium: Aquarium): Aquarium
+
+  def addInhabitant[A](aquarium: Aquarium, inhabitant: A): Aquarium
+
+  def removeInhabitant[A](aquarium: Aquarium, inhabitant: A): Aquarium
+
+  def addFood[A](aquarium: Aquarium, food: A): Aquarium
+
+  def removeFood[A](aquarium: Aquarium, food: A): Aquarium
+
+  def step(aquarium: Aquarium): Aquarium

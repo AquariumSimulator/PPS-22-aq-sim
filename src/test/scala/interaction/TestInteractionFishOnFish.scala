ef115c914dfddd01f7fc336b3d812f292b35ddb0
Pedoni <emanuele.lamagna@studio.unibo.it>
2022-09-19 22:46:24 +0200

    Formatted test

--- src/test/scala/interaction/TestInteractionFishOnFish.scala
+++ src/test/scala/interaction/TestInteractionFishOnFish.scala
@@ -7,33 +7,33 @@ import model.interaction.Interaction
 
 class TestInteractionFishOnFish extends AnyFunSpec:
 
-    private val carnivorousFish =
-        Fish(feedingType = FeedingType.CARNIVOROUS, hunger = 70)
+  private val carnivorousFish =
+    Fish(feedingType = FeedingType.CARNIVOROUS, hunger = 70)
 
-    private val herbivorousFish =
-        Fish(feedingType = FeedingType.HERBIVOROUS, size = Fish.MAX_SIZE)
+  private val herbivorousFish =
+    Fish(feedingType = FeedingType.HERBIVOROUS, size = Fish.MAX_SIZE)
 
-    private val interaction = Interaction(carnivorousFish, herbivorousFish)
+  private val interaction = Interaction(carnivorousFish, herbivorousFish)
 
-    describe("A carnivorous fish") {
-        it("should eat an herbivorous fish when he is hungry") {
-            val tuple = interaction.update()
-            val fullFish = tuple._1
-            val deadFish = tuple._2
-            assert(fullFish.get.hunger == 95)
-            assert(deadFish.get.hunger == 0)
-        }
-
-        it("shouldn't eat an herbivorous fish when he isn't hungry") {
-            val newCarnivorous =
-              Fish(feedingType = FeedingType.CARNIVOROUS, hunger = 90)
-            val newHerbivorous =
-              Fish(feedingType = FeedingType.HERBIVOROUS, size = Fish.MAX_SIZE)
-            var newInteraction = Interaction(newCarnivorous, newHerbivorous)
-            var tuple = newInteraction.update()
-            assert(tuple === (Option.empty, Option.empty))
-            newInteraction = Interaction(newHerbivorous, newCarnivorous)
-            tuple = newInteraction.update()
-            assert(tuple === (Option.empty, Option.empty))
-        }
+  describe("A carnivorous fish") {
+    it("should eat an herbivorous fish when he is hungry") {
+      val tuple = interaction.update()
+      val fullFish = tuple._1
+      val deadFish = tuple._2
+      assert(fullFish.get.hunger == 95)
+      assert(deadFish.get.hunger == 0)
     }
+
+    it("shouldn't eat an herbivorous fish when he isn't hungry") {
+      val newCarnivorous =
+        Fish(feedingType = FeedingType.CARNIVOROUS, hunger = 90)
+      val newHerbivorous =
+        Fish(feedingType = FeedingType.HERBIVOROUS, size = Fish.MAX_SIZE)
+      var newInteraction = Interaction(newCarnivorous, newHerbivorous)
+      var tuple = newInteraction.update()
+      assert(tuple === (Option.empty, Option.empty))
+      newInteraction = Interaction(newHerbivorous, newCarnivorous)
+      tuple = newInteraction.update()
+      assert(tuple === (Option.empty, Option.empty))
+    }
+  }

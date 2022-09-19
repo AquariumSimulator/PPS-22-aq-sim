aaf78ed604b983f793c1b28650418ef815f1a550
Elisa Albertini <elisa.albertini3@studio.unibo.it>
2022-09-19 16:26:19 +0200

    Added and implemented SimulationEngine.

---
+++ src/main/scala/controller/SimulationEngine.scala
@@ -0,0 +1,28 @@
+package controller
+
+import mvc.ControllerModule.Requirements
+
+trait SimulationEngine:
+
+  def start(): Unit
+
+  def stop(): Unit
+
+  def resume(): Unit
+
+object SimulationEngine:
+
+  def apply(context: Requirements): SimulationEngine = new SimulationEngineImpl(context)
+
+  private class SimulationEngineImpl(context: Requirements) extends SimulationEngine:
+
+    val aquarium = context.model.initializeAquarium(1, 1, 1) //by user
+
+    override def start(): Unit =
+      Iterator.iterate(aquarium)(context.model.step).foreach( aq =>
+        Thread.sleep(2000)
+        println("Fish -> " + aq.population.herbivorous.head.position)
+      )
+    override def stop(): Unit = ???
+
+    override def resume(): Unit = ???

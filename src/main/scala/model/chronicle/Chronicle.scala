4aca20f052b797b22f2f2aac3484b2ca6effcbac
Pedoni <emanuele.lamagna@studio.unibo.it>
2022-10-03 21:15:33 +0200

    Merged develop and fixed spacing

--- src/main/scala/model/chronicle/Chronicle.scala
+++ src/main/scala/model/chronicle/Chronicle.scala
@@ -1,8 +1,8 @@
 package model.chronicle
 
 /** This class represent the chronicle of the aquarium events
- *
- * @param events
- *   represent the list of the events
- */
+  *
+  * @param events
+  *   represent the list of the events
+  */
 case class Chronicle(events: List[String] = List())

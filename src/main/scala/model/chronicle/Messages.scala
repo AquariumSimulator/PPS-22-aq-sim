43919ac78682df9d405ec4354daf1b8276d23b5a
Elisa Albertini <elisa.albertini3@studio.unibo.it>
2022-10-07 12:17:15 +0200

    Refactoring; added function that return the name of a entity.

--- src/main/scala/model/chronicle/Messages.scala
+++ src/main/scala/model/chronicle/Messages.scala
@@ -1,23 +1,34 @@
 package model.chronicle
+import model.Algae
+import model.Entity
+import model.food.Food
+import model.fish.Fish
 
 object Messages:
+  private def checkEntity[A](entity: A): String =
+    entity match
+      case f: Fish => f.name
+      case _: Algae => "Alga"
+      case _: Food => "Food"
 
   val CLEAN_AQUARIUM: String = "Aquarium was cleaned"
 
   val UPDATED_AQUARIUM_STATE_PARAMETER: (Double, String, String) => String =
-    (value: Double, name: String, unitOfMeasure: String) => name + " was updated: " + value + unitOfMeasure
+    (value: Double, name: String, unitOfMeasure: String) =>
+      val round = BigDecimal(value).setScale(2, BigDecimal.RoundingMode.HALF_UP).toDouble
+      name + " was updated: " + round + " " + unitOfMeasure
 
-  val ADDED_ENTITY: String => String =
-    (entity: String) => entity + " was added to the aquarium"
+  def ADDED_ENTITY[A]: A => String =
+    (entity: A) => checkEntity(entity) + " was added to the aquarium"
 
-  val REMOVED_ENTITY: String => String =
-    (entity: String) => entity + " was removed from the aquarium"
+  def REMOVED_ENTITY[A]: A => String =
+    (entity: A) => checkEntity(entity) + " was removed from the aquarium"
 
   val FISH_BIRTH: String => String =
     (name: String) => name + " was born"
 
-  val ENTITY_DEATH: String => String =
-    (entity: String) => entity + "was dead"
+  def ENTITY_DEATH[A]: A => String =
+    (entity: A) => checkEntity(entity) + " was dead"
 
   val CHANGED_SPEED: String => String =
     (speed: String) => "Simulation speed updated: " + speed

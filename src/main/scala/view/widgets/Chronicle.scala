108fd954e9f6f2a91f40f173e8081c8deae26cd9
Pedoni <emanuele.lamagna@studio.unibo.it>
2022-10-09 17:58:35 +0200

    Added ChronicleEvents but still doesn't work

--- src/main/scala/view/widgets/Chronicle.scala
+++ src/main/scala/view/widgets/Chronicle.scala
@@ -2,20 +2,21 @@ package view.widgets
 
 import scalafx.geometry.{Insets, Pos}
 import scalafx.scene.control.{Label, ListView}
-import scalafx.scene.layout._
+import scalafx.scene.layout.*
 import scalafx.scene.paint.Color
 import view.utils.AquariumFonts
 import mvc.MVC
 import mvc.MVC.given_ViewRequirements as context
 import mvc.ViewModule.ViewRequirements
+import view.widgets.slider.ChronicleEvents
 
 object Chronicle:
 
-  var list: ListView[String] = new ListView(
-    items = Seq()
-  )
+  var list: ListView[String] = new ListView(items = List())
   list.editable = true
   list.maxHeight = 150
+
+  //val list = new ChronicleEvents()
 
   val chronicle = new BorderPane:
     margin = Insets(5, 0, 0, 0)

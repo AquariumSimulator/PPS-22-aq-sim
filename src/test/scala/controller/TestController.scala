6fea95727e66d08ed6324572ed535daf26f2231f
Elisa Albertini <elisa.albertini3@studio.unibo.it>
2022-10-10 16:43:48 +0200

    Added tests

--- src/test/scala/controller/TestController.scala
+++ src/test/scala/controller/TestController.scala
@@ -14,15 +14,14 @@ class TestController extends AnyFunSpec:
   private val oxygenation = 3
 
   private val food = Food()
-  private val algae = Algae()
   private val fish = Fish()
 
   private val testEvent = "Test event"
 
   private def update(aq: Aquarium): Aquarium = model.step(aq)
 
-  describe("Given the interface Controller"){
-    describe("when updateTemperature is called and a step is executed"){
+  describe("Given the interface Controller") {
+    describe("when updateTemperature is called and a step is executed") {
       it(s"should return a new aquarium with the temperature equal to $temperature") {
         controller.updateTemperature(temperature)
         assert(update(controller.getAquarium).aquariumState.temperature == temperature)
@@ -50,6 +49,46 @@ class TestController extends AnyFunSpec:
       }
     }
 
+    describe(s"when addInhabitant is called to add $fish and a step is executed") {
+      it(s"should return a new aquarium that contains $fish") {
+        controller.addInhabitant(fish)
+        val aq = update(controller.getAquarium)
+        assert(aq.population.fish.contains(fish))
+        assert(aq.population.carnivorous.contains(fish))
+      }
+    }
+
+    describe(s"and when removeInhabitant is called to remove $fish and a step is executed") {
+      it(s"should return a new aquarium that doesn't contain $fish") {
+        controller.addInhabitant(fish)
+        val aq = update(controller.getAquarium)
+        controller.removeInhabitant(fish)
+        val newAq = update(aq)
+        assert(!newAq.population.fish.contains(fish))
+        assert(!newAq.population.carnivorous.contains(fish))
+      }
+    }
+
+    describe(s"when addFood is called to add $food and a step is executed") {
+      it(s"should return a new aquarium that contains $food") {
+        controller.addFood(food)
+        val aq = update(controller.getAquarium)
+        assert(aq.availableFood.contains(food))
+        assert(aq.carnivorousFood.contains(food))
+      }
+    }
+
+    describe(s"and when deleteFood is called to remove $food and a step is executed") {
+      it(s"should return a new aquarium that doesn't contain $food") {
+        controller.addFood(food)
+        val aq = update(controller.getAquarium)
+        controller.deleteFood(food)
+        val newAq = update(aq)
+        assert(!newAq.availableFood.contains(food))
+        assert(!newAq.carnivorousFood.contains(food))
+      }
+    }
+
     describe("when getPopulationTrend is called") {
       it("should return the population of the aquarium at each iteration") {
         assert(controller.getPopulationTrend.head._1 == InitializeAquarium.HERBIVOROUS_FISH)
@@ -66,4 +105,3 @@ class TestController extends AnyFunSpec:
       }
     }
   }
-

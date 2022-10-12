4d06951d6341cd832bf7f9670689a83639b89bb0
Elisa Albertini <elisa.albertini3@studio.unibo.it>
2022-10-12 13:44:09 +0200

    Refactoring

--- src/main/scala/model/interaction/aquariumAlgae/InteractionAquariumOnAlgaeImpl.scala
+++ src/main/scala/model/interaction/aquariumAlgae/InteractionAquariumOnAlgaeImpl.scala
@@ -23,21 +23,13 @@ class InteractionAquariumOnAlgaeImpl(algae: Algae, aquariumState: AquariumState)
       Option.empty
     else Some(algae.copy(height = algae.height + calculateAlgaeGrowth()))
 
-  /** Check if the algae is dead due to the lack of brightness
-    * @return
-    *   true if the algae is dead otherwise it returns false
-    */
-  def checkIfAlgaeAreDead(): Boolean =
+  private def checkIfAlgaeAreDead(): Boolean =
     aquariumState.brightness match
       case brightnessLevel if brightnessLevel <= DeathProbabilityAlgae.MAX_BRIGHTNESS_LEVEL_PROB_INTERVAL =>
         Random.between(0, 100) < DeathProbabilityAlgae.LACK_OF_BRIGHTNESS(brightnessLevel)
       case _ => false
 
-  /** Calculate the growth of the algae thanks the level of brightness
-    * @return
-    *   the growth
-    */
-  def calculateAlgaeGrowth(): Int =
+  private def calculateAlgaeGrowth(): Int =
     val calculatePercentage = (b: Int) =>
       Algae.MIN_GROWTH +
         ((b - Algae.MIN_GROWTH) * (Algae.MAX_GROWTH - Algae.MIN_GROWTH)) /

9b6560e0c44db3d65cbbb5f5b593b169e6e17178
Pedoni <emanuele.lamagna@studio.unibo.it>
2022-10-14 11:58:49 +0200

    Removed useless onAction event parameters

--- src/main/scala/view/widgets/BottomBar.scala
+++ src/main/scala/view/widgets/BottomBar.scala
@@ -24,14 +24,13 @@ object BottomBar:
 
   val addFishButton: BottomBarButton = BottomBarButton("/icons/add-fish.png")
   addFishButton.tooltip = new Tooltip("Add fish or algae")
-  addFishButton.onAction = (event: ActionEvent) =>
+  addFishButton.onAction = _ =>
     val dialog: ChoiceDialog[String] = new ChoiceDialog(
       defaultChoice = "Herbivorous Fish",
       choices = Seq("Herbivorous Fish", "Carnivorous Fish", "Algae")
     ):
       title = "Add fish or algae"
       headerText = "Choose the type"
-    // initStyle(StageStyle.Utility)
     val choice: Option[String] = dialog.showAndWait()
 
     val newInhabitant = choice match
@@ -53,7 +52,7 @@ object BottomBar:
   val changeSpeedButton: BottomBarButton = BottomBarButton("/icons/speed2.png")
   changeSpeedButton.disable = true
   changeSpeedButton.tooltip = Tooltip("Increase speed")
-  changeSpeedButton.onAction = (event: ActionEvent) =>
+  changeSpeedButton.onAction = _ =>
     context.controller.getSpeed match
       case SimulationSpeed.SLOW =>
         context.controller.changeSpeed(SimulationSpeed.NORMAL)
@@ -87,7 +86,7 @@ object BottomBar:
   val playButton: BottomBarButton = BottomBarButton("/icons/play.png")
   playButton.tooltip = Tooltip("Play the simulation")
   var speedBeforeStop: SimulationSpeed = SimulationSpeed.NORMAL
-  playButton.onAction = (event: ActionEvent) =>
+  playButton.onAction = _ =>
     context.controller.isRunning match
       case true =>
         speedBeforeStop = context.controller.getSpeed
@@ -113,14 +112,13 @@ object BottomBar:
 
   val foodButton: BottomBarButton = BottomBarButton("/icons/food.png")
   foodButton.tooltip = new Tooltip("Add food")
-  foodButton.onAction = (event: ActionEvent) =>
+  foodButton.onAction = _ =>
     val dialog: ChoiceDialog[String] = new ChoiceDialog(
       defaultChoice = "Herbivorous Food",
       choices = Seq("Herbivorous Food", "Carnivorous Food")
     ):
       title = "Add food"
       headerText = "Choose the food type"
-    // initStyle(StageStyle.Utility)
     val choice: Option[String] = dialog.showAndWait()
     val food = choice match
       case Some(str) if str == "Herbivorous Food" => Some(Food(feedingType = FeedingType.HERBIVOROUS))
@@ -131,7 +129,7 @@ object BottomBar:
 
   val cleanButton: BottomBarButton = BottomBarButton("/icons/clean.png")
   cleanButton.tooltip = Tooltip("Clean the aquarium")
-  cleanButton.onAction = (event: ActionEvent) => context.controller.clean()
+  cleanButton.onAction = _ => context.controller.clean()
 
   val bottomBar: TilePane =
     new TilePane:

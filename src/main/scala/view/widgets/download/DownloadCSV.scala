dc177ff233322b3ef8ad1b5d7ca93332022e97a3
Filippo Benvenuti <filippo.benvenuti3@studio.unibo.it>
2022-10-21 10:11:37 +0200

    download csv refactor

--- src/main/scala/view/widgets/download/DownloadCSV.scala
+++ src/main/scala/view/widgets/download/DownloadCSV.scala
@@ -6,35 +6,32 @@ import scala.jdk.CollectionConverters._
 import com.github.tototoshi.csv.CSVWriter
 import com.github.tototoshi.csv.defaultCSVFormat
 import mvc.MVC.given_ViewRequirements as context
+import model.fish.Fish
+import model.Algae
 
 /** Used to save to file data of simulation in CSV format. */
 object DownloadCSV:
 
   def apply(path: String): Unit =
+    given String = path
     println("Saving fishes...")
-    saveFish(path)
-    println("Saving algaes...")
-    saveAlgae(path)
-    println("Done: " + path)
-
-  private def saveFish(path: String) =
-    saveToCSV(path + "fish.csv")(context.controller.getAllFish)(fish =>
+    saveToCSV("fish.csv")(context.controller.getAllFish)(fish =>
       List(
         fish.feedingType.toString,
         fish.name
       )
     )
-
-  private def saveAlgae(path: String) =
-    saveToCSV(path + "algae.csv")(context.controller.getAllAlgae)(algae =>
+    println("Saving algaes...")
+    saveToCSV("algae.csv")(context.controller.getAllAlgae)(algae =>
       List(
         algae.base.toString,
         algae.height.toString
       )
     )
+    println("Done: " + path)
 
-  private def saveToCSV[U](pathWithName: String)(getList: Int => List[U])(mapper: U => List[String]) =
-    val outputFile = BufferedWriter(FileWriter(pathWithName))
+  private def saveToCSV[U](using path: String)(name: String)(getList: Int => List[U])(mapper: U => List[String]) =
+    val outputFile = BufferedWriter(FileWriter(path + name))
     val csvWriter = CSVWriter.open(outputFile)
     (0 to context.controller.currentIteration).foreach(iteration =>
       csvWriter.writeAll(
